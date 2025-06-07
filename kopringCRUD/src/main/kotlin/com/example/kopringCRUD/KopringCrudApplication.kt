01a0cf80d09941baf90ff553d708fc9d9a2ff20c
양희범 <sinbla78@gmail.com>
2025-06-07 14:27:18 +0900

    feat :: 프로젝트 세팅

---
+++ kopringCRUD/src/main/kotlin/com/example/kopringCRUD/KopringCrudApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kopringCRUD
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringCrudApplication
+
+fun main(args: Array<String>) {
+	runApplication<KopringCrudApplication>(*args)
+}
---
+++ kopringCRUD/src/test/kotlin/com/example/kopringCRUD/KopringCrudApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kopringCRUD
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringCrudApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
