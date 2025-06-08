affa24b4d3fea8651efce2514a3f344a42001ac4
양희범 <sinbla78@gmail.com>
2025-06-08 16:40:13 +0900

    feat :: cors 추가

--- kopringCRUD/src/main/kotlin/com/example/kopringCRUD/global/security/SecurityConfig.kt
+++ kopringCRUD/src/main/kotlin/com/example/kopringCRUD/global/security/SecurityConfig.kt
@@ -10,6 +10,8 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.firewall.HttpFirewall
+import org.springframework.security.web.firewall.StrictHttpFirewall
 import org.springframework.web.cors.CorsConfigurationSource
 
 @Configuration
@@ -49,5 +51,12 @@ class SecurityConfig(
             }
 
         return http.build()
+    }
+
+    @Bean
+    fun httpFirewall(): HttpFirewall {
+        val firewall = StrictHttpFirewall()
+        firewall.setAllowUrlEncodedDoubleSlash(true)
+        return firewall
     }
 }
